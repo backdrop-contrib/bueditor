8f3fd751c6dd042113729be2bde931b2464de10b
ufku <ufku@9910.no-reply.drupal.org>
2009-07-11 17:08:47 +0000

    - Revised editor popup so that jquery or html objects could be sent as content while opening.

--- bueditor.js
+++ bueditor.js
@@ -26,25 +26,23 @@ BUE.behavior = function(context) {
   $.each(BUE.preset, function (tid, tplid) {
     BUE.processTextarea($('#'+ tid, context).get(0), tplid);
   });
+  //fix enter key on autocomplete fields triggering button click.
+  $('input.form-autocomplete', context).keydown(function(e) {return e.keyCode != 13});
 };
 
 //editor settle.
 BUE.initiate = function () {
-  (Drupal.behaviors.BUE = BUE.behavior)(document);
   //set editor quickPop.
-  var qp = BUE.quickPop = BUE.createPopup('bue-quickpop');
-  $(qp.rows[0]).hide();
-  qp.oldopen = qp.open;
-  qp.open = function(content, effect) {
-    qp.oldopen(null, content, effect);
-    $(document).mouseup(qpEnd);
-    function qpEnd() {qp.close(); $(document).unbind('mouseup', qpEnd);}
-  };
-  //set editor dialog
+  var Q = BUE.quickPop = BUE.createPopup('bue-quickpop');
+  var endQ = function() {Q.close(); $(document).unbind('mouseup', endQ);};
+  Q.oldopen = Q.open;
+  Q.open = function(content, effect) {Q.oldopen(null, content, effect);$(document).mouseup(endQ);};
+  $('.head:first', Q).hide();
+  //set editor dialog.
   BUE.dialog.popup = BUE.createPopup('bue-dialog');
   BUE.dialog.popup.close = function (effect) {BUE.dialog.close(effect);}
-  //fix enter key on autocomplete fields triggering button click.
-  $('input.form-autocomplete').keydown(function(e) {return e.keyCode != 13});
+  //set drupal behavior.
+  (Drupal.behaviors.BUE = BUE.behavior)(document);
 };
 
 //integrate editor template into textarea T
@@ -86,11 +84,10 @@ BUE.processTextarea = function (T, tplid) {
 
 //create an editor instance
 BUE.instance = function (T, tplid) {
-  var E = this;
-  E.index = BUE.instances.length;
+  var i = BUE.instances.length, E = T.editor = BUE.instances[i] = this;
+  E.index = i;
   E.textArea = T;
-  E.textArea.editor = BUE.instances[E.index] = E;
-  E.tpl = BUE.templates[tplid];
+  E.tpl = BUE.templates[E.tplid = tplid];
   E.UI = $(BUE.theme(tplid).replace(/\%n/g, E.index)).insertBefore(T);
   E.buttons = $('input.bue', E.UI).get();
   E.bindex = null;
@@ -198,74 +195,72 @@ BUE.theme = function (tplid) {
   return ET.html;
 };
 
-//general popup&dialog html
-BUE.popHtml = '<table class="editor-popup" id="%id" style="position: absolute; display: none;"><tbody><tr class="head even"><td class="title">%tt</td><td class="close"><a>x</a></td></tr><tr class="body odd"><td colspan="2" class="cnt">%ct</td></tr></tbody></table>';
+//default template for editor popups or dialogs.
+BUE.popHtml = '<table class="editor-popup" style="position: absolute; display: none;"><tbody><tr class="head even"><td class="title"></td><td class="close"><a>x</a></td></tr><tr class="body odd"><td colspan="2" class="cnt"></td></tr></tbody></table>';
 
-//create/open editor popup object
+//open popup.
 BUE.openPopup = function (id, title, content, effect) {
-  var popup = BUE.createPopup(id);
-  popup.open(title, content, effect);
-  return popup;
+  return BUE.createPopup(id).open(title, content, effect);
 }
+
+//create popup
 BUE.createPopup = function (id, title, content) {
   if (BUE.popups[id]) {
     return BUE.popups[id];
   }
-  var html = BUE.popHtml.replace('%id', id).replace('%tt', title||'').replace('%ct', content||'');
-  var popup = BUE.popups[id] = $(html).appendTo(document.body).get(0);
-  $(popup.rows[0]).mousedown(function (e) {
-    var X = e.pageX;
-    var Y = e.pageY;
-    var pos = $(popup).offset();
-    $(document).mousemove(doDrag).mouseup(endDrag);
-    function doDrag(e) {
-      popup.style.left = (pos.left+e.pageX-X) +'px';
-      popup.style.top = (pos.top+e.pageY-Y) +'px';
-      return false;
-    }
-    function endDrag(e) {
-      $(document).unbind('mousemove', doDrag).unbind('mouseup', endDrag);
-    }
+  var P = BUE.popups[id] = $(BUE.popHtml).appendTo('body').attr('id', id).find('.title:first').html(title || '').end().find('.cnt:first').html(content || '').end().get(0);
+  //drag
+  $('.head:first', P).mousedown(function (e) {
+    var X = e.pageX, Y = e.pageY, pos = $(P).offset();
+    var drag =  function(e) {$(P).css({left: pos.left + e.pageX - X, top: pos.top + e.pageY - Y});return false;};
+    var undrag = function(e) {$(document).unbind('mousemove', drag).unbind('mouseup', undrag)};
+    $(document).mousemove(drag).mouseup(undrag);
   });
-  $(popup.rows[0].cells[1].firstChild).click(function() {popup.close();});
-  popup.open = function (title, content, effect) {
-    if (typeof(title) == 'string') this.rows[0].cells[0].innerHTML = title;
-    if (typeof(content) == 'string') this.rows[1].cells[0].innerHTML = content;
+  //close
+  P.close = function (effect) {$(P)[effect || 'hide']()};
+  $('.close:first', P).click(function() {P.close()});
+  //open
+  P.open = function (title, content, effect) {
     var pos = $(BUE.active.buttons[BUE.active.bindex]).offset();
-    this.style.left = pos.left-20 +'px';
-    this.style.top = pos.top+10 +'px';
-    this.editor = BUE.active;
-    $(this)[effect||'show']();
+    $(P).css({left: pos.left - 20, top: pos.top + 10});
+    P.editor = BUE.active;
+    if (typeof title != 'undefined' && title != null) {
+      $('.title:first', P).html(title);
+    }
+    if (typeof content != 'undefined' && content != null) {
+      $('.cnt:first', P).html(content);
+    }
+    $(P)[effect || 'show']();
+    return P;
   };
-  popup.close = function (effect) {
-    $(this)[effect||'hide']();
-  };
-  return popup;
+  return P;
 };
 
 //dialog functions
 BUE.dialog.open = function (title, content, effect) {
-  if (this.editor) this.close();
-  this.editor = BUE.active;
-  this.editor.buttonsDisabled(true);
-  $(this.editor.buttons[this.editor.bindex]).addClass('stay-clicked');
-  this.esp = this.editor.posSelection();
-  this.popup.open(title, content, effect);
-  this.oldfocus = this.editor.textArea.onfocus;
-  this.editor.textArea.onfocus = function () {this.blur();};
+  var D = BUE.dialog;
+  if (D.editor) D.close();
+  var E = D.editor = BUE.active;
+  E.buttonsDisabled(true);
+  $(E.buttons[E.bindex]).addClass('stay-clicked');
+  D.esp = E.posSelection();
+  D.popup.open(title, content, effect);
+  D.oldfocus = E.textArea.onfocus;
+  E.textArea.onfocus = function () {this.blur();};
 };
 BUE.dialog.close = function (effect) {
-  if (this.editor) {
-    this.editor.textArea.onfocus = this.oldfocus;
-    this.editor.buttonsDisabled(false);
-    $(this.editor.buttons[this.editor.bindex]).removeClass('stay-clicked');
-    if (this.editor == BUE.active) {// restore previous states
-      if (BUE.mode == 2) this.editor.makeSelection(this.esp.start, this.esp.end); // selection for IE
-      else this.editor.focus(); // focus for FF
+  var E, D = BUE.dialog;
+  if (E = D.editor) {
+    E.textArea.onfocus = D.oldfocus;
+    E.buttonsDisabled(false);
+    $(E.buttons[E.bindex]).removeClass('stay-clicked');
+    if (E.index == BUE.active.index) {// restore previous states
+      if (BUE.mode == 2) E.makeSelection(D.esp.start, D.esp.end); // selection for IE
+      else E.focus(); // focus for FF
     }
-    this.editor = null;
-    this.esp = null;
-    $(this.popup)[effect||'hide']();
+    D.editor = null;
+    D.esp = null;
+    $(D.popup)[effect || 'hide']();
   }
 };
 
@@ -287,19 +282,14 @@ BUE.selMake = function (T, start, end) {
 if (BUE.mode == 2) {
   BUE.selPos = function (T) {
     T.focus();
-    var val = T.value.bueText();
-    var mark = '~`^'; //dummy text.
-    for (var i = 0; val.indexOf(mark) != -1; i++) mark += mark.charAt(i); //make sure mark is unique.
-    var mlen = mark.length;
-    var range = document.selection.createRange();
-    var bm = range.getBookmark();
-    var slen = range.text.bueText().length;
+    var i, val = T.value.bueText(), mark = '~`^'; //dummy text.
+    for (i = 0; val.indexOf(mark) != -1; i++) mark += mark.charAt(i); //make sure mark is unique.
+    var mlen = mark.length, range = document.selection.createRange();
+    var bm = range.getBookmark(), slen = range.text.bueText().length;
     range.text = mark;
-    var tmp = T.value.bueText();
-    var start = tmp.indexOf(mark);
-    for (var i = 0; tmp.charAt(start+i+mlen) == '\n'; i++);
-    var end = start+slen;
-    for (; val.charAt(end) == '\n'; end++);
+    var tmp = T.value.bueText(), start = tmp.indexOf(mark);
+    for (i = 0; tmp.charAt(start+i+mlen) == '\n'; i++);
+    for (var end = start+slen; val.charAt(end) == '\n'; end++);
     end -= i;
     T.value = val;
     if (start == end && !val.charAt(end)) range.collapse(false);//bookmark has problems with a cursor at the end
