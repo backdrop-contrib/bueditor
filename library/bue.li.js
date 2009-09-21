f23b1a6a3ae4bd9140ca5f76c906872dd15bc259
ufku <ufku@9910.no-reply.drupal.org>
2009-09-21 23:06:12 +0000

    - new library for auto inserting html list items.

---
+++ library/bue.li.js
@@ -0,0 +1,15 @@
+// $Id$
+
+//Automatically insert a new list item when enter-key is pressed at the end of a list item.
+BUE.postprocess.push(function(E) {
+
+  $(E.textArea).keyup(function(e) {
+    if (!e.ctrlKey && !e.shiftKey && !e.originalEvent.altKey && e.keyCode == 13) {
+      var text = E.getContent().substr(0, E.posSelection().start);
+      if (text.search(/<\/li>\s*$/) != -1) {
+        E.tagSelection('<li>', '</li>');
+      }
+    }
+  });
+ 
+});
