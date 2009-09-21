9bc5f1686cef2251865f8cb56e027d579cc8c7ac
ufku <ufku@9910.no-reply.drupal.org>
2009-09-21 23:02:01 +0000

    - new library for adding Ctrl shortcuts.

---
+++ library/bue.ctrl.js
@@ -0,0 +1,28 @@
+// $Id$
+
+//Register button accesskeys as Ctrl shortcuts.
+BUE.postprocess.push(function(E) {
+
+  //store key-button relations.
+  E.ctrlKeys = {};
+
+  //get button keys
+  $.each(E.buttons, function(i, B) {
+    if (k = E.tpl.buttons[B.bid][3]) {
+      E.ctrlKeys[k.toUpperCase().charCodeAt(0)] = B;
+    }
+  });
+
+  //register ctrl shortcuts for the editor.
+  $(E.textArea).keydown(function(e) {
+    if (e.ctrlKey && !e.shiftKey && !e.originalEvent.altKey && E.ctrlKeys[e.keyCode]) {
+      E.ctrlKeys[e.keyCode].click();
+      return false;
+    }
+  });
+
+});
+
+
+//Extend or alter shortcuts in your own postprocess:
+//E.ctrlKeys['YOUR_KEY_CODE'] = {click: YOUR_CALLBACK};
