8e301bc8f7dfb2bcfb368e81fbb6187287c7b37c
ufku <ufku@9910.no-reply.drupal.org>
2009-09-21 23:48:35 +0000

    - make popups close on ESC. - allow link focusing in quickpop with UP and DOWN keys and triggerring click on ENTER.

---
+++ library/bue.accessibility.js
@@ -0,0 +1,29 @@
+// $Id$
+
+//Accessibility improvements.
+BUE.postprocess.push(function(E) {
+  if (E.index) return;//run once
+  //Close quickpop by pressing ESC or ENTER key.
+  //Allow link focusing(ARROW KEYS) and triggering(ENTER) inside quickpop.
+  var Q = BUE.quickPop, Qo = Q.open, D = BUE.dialog;
+  Q.open = function(content, effect) {
+    Qo(content, effect);
+    setTimeout(function(){$(Q).find('a:first').focus()});
+    return Q;
+  };
+  $(Q).attr('tabindex', 0).keydown(function (e) {
+    switch (e.keyCode) {
+      case 13:case 27: 
+        setTimeout(function(){Q.close().bue.focus()});
+        break;
+      case 38:case 40:
+        var $a = $('a', BUE.quickPop), i = $a.index(document.activeElement);
+        $a.eq(i+e.keyCode-39).focus();
+        return false;
+    }
+  });
+  //Make dialogs close on ESC too.
+  $(D).attr('tabindex', 0).keydown(function (e) {
+    e.keyCode == 27 && D.close();
+  });
+});
