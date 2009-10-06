6cd9b6ef3583a4358c16b5a1d31c97403e559fda
ufku <ufku@9910.no-reply.drupal.org>
2009-10-06 20:49:15 +0000

    - make history reusable by BUE.history

--- library/bue.history.js
+++ library/bue.history.js
@@ -1,76 +1,84 @@
 // $Id$
-
 //Introduces cross-browser editor history with two new methods. E.undo() & E.redo()
-BUE.postprocess.push(function(E, $) {
+(function(E, $) {
 
-  var H = E.history = {
-    max: 50, //maximum number of states in undo/redo history
-    keys: {'13': 1, '32': 1}, //the key codes(not char codes) triggering state saving. (13:enter, 32:space)
-    period: 2000, //minimum time needed to pass before saving successively triggered states.
-    states: [], //stores the states
-    current: -1, //index of the latest activated/stored state
-    writable: true //dynamic allowance of state saving.
-  };
-
-  //allow write permission
-  H.allow = function(){H.writable = true};
-
-  //save textarea state.
-  H.save = function(bypass) {
-    //chek write perm
-    if (!bypass && !H.writable) {
-      return;
-    }
-    H.writable = false;
-    setTimeout(H.allow, H.period);
-    //delete redo-states if any.
-    while(H.states[H.current + 1]) {
-      H.states.pop();
-    }
-    var V = E.getContent(), len = H.states.length;
-    if (len && V == H.states[len-1].value) {
-      return;
-    }
-    if (len == H.max) {
-      H.states.shift();
-      len--;
-    }
-    H.states[(H.current = len)] = {value: V, cursor: E.posSelection()};
-  };
-  
-  //restore a state relative to the current state.
-  H.go = function(i) {
-    if (i && i < 0 && H.current == H.states.length - 1) {
-      H.save(true);
-    }
-    var state, index = H.current + (i || 0);
-    if (state = H.states[index]) {
-      E.textArea.value = state.value;//not using setContent() since it triggers state saving.
-      E.makeSelection(state.cursor.start, state.cursor.end);
-      H.current = index;
-    }
-  };
+//history object
+BUE.history = function(E) {
+  var H = this;
+  H.bue = E;
+  H.max= 50; //maximum number of states in undo/redo history
+  H.keys= {'13': 1, '32': 1}; //the key codes(not char codes) triggering state saving. (13:enter, 32:space)
+  H.period= 2000; //minimum time needed to pass before saving successively triggered states.
+  H.states= []; //stores the states
+  H.current= -1; //index of the latest activated/stored state
+  H.writable= true; //dynamic allowance of state saving.
   
   //attach textarea events triggering history operations.
-  $(E.textArea).one('focus', H.save).keyup(function(e) {
-    if (H.writable && H.keys[e.keyCode]) {
-      H.save();
-    }
+  $(E.textArea).one('focus', function(){H.save()}).keyup(function(e) {
+    H.writable && H.keys[e.keyCode] && H.save();
   });
   
   //save history on setContent.
-  H._setContent = E.setContent;
+  E.historySetContent = E.setContent;
   E.setContent = function(content) {
     H.save();
-    return H._setContent(content);
+    return E.historySetContent(content);
   };
+};
 
-  //undo/redo for the editor.
-  E.undo = function() {H.go(-1)};
-  E.redo = function() {H.go(1)};
+//history methods
+var H = BUE.history.prototype;
+
+//allow/disallow write permission
+H.allow = function(){this.writable = true};
+H.disallow = function(){this.writable = false};
+
+//save textarea state.
+H.save = function(bypass) {
+  var H = this, E = H.bue;
+  //chek write perm
+  if (!bypass && !H.writable) {
+    return;
+  }
+  H.disallow();
+  setTimeout(function(){H.allow()}, H.period);
+  //delete redo-states if any.
+  while(H.states[H.current + 1]) {
+    H.states.pop();
+  }
+  var val = E.getContent(), len = H.states.length;
+  if (len && val == H.states[len-1].value) {
+    return;
+  }
+  if (len == H.max) {
+    H.states.shift();
+    len--;
+  }
+  H.states[(H.current = len)] = {value: val, cursor: E.posSelection()};
+};
+
+//restore a state relative to the current state.
+H.go = function(i) {
+  var H = this, E = H.bue;
+  i < 0 && H.current == H.states.length - 1 && H.save(true);
+  var state, index = H.current + i;
+  if (state = H.states[index]) {
+    E.textArea.value = state.value;//not using setContent() since it triggers state saving.
+    E.makeSelection(state.cursor.start, state.cursor.end);
+    H.current = index;
+  }
+};
   
+//undo/redo for the editor.
+E.undo = function() {this.history.go(-1)};
+E.redo = function() {this.history.go(1)};
+
+//create history for each editor instance
+BUE.postprocess.push(function(E, $) {
+  E.history = new BUE.history(E);
 });
 
+})(BUE.instance.prototype, jQuery);
 
 //Change settings in your own postprocess.
 //E.history.max = YOUR_MAXIMUM_NUMBER_OF_UNDO_STATES;
