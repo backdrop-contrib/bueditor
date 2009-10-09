606d9b8a1dc0ce76760c74f205aa42cc81680bea
ufku <ufku@9910.no-reply.drupal.org>
2009-10-09 22:08:28 +0000

    - jquery no conflict

--- admin/bueditor.eop.js
+++ admin/bueditor.eop.js
@@ -2,14 +2,14 @@
 
 //Add editor name prompt for copy and add_default operations.
 //Confirm editor deletion without going to the confirmation page.
-$(function() {
+(function($) {
 
+$(document).ready(function() {
   $('a.eop-copy, a.eop-add-default').click(function() {
     var name = prompt(Drupal.t('Editor name'), this.name);
     if (name) location.replace(this.href + '&name=' + name);
     return false;
   }).add('a.eop-export').removeClass('active');
-
   $('a.eop-delete').click(function() {
     var lnk = $(this).nextAll('a.eop-copy')[0];
     var msg = Drupal.t('Are you sure you want to delete the editor @name?', {'@name': lnk.name.substr(8)}) +'\n';
@@ -20,5 +20,6 @@ $(function() {
     }
     return false;
   });
-
 });
+
+})(jQuery);
