0aca09e9f7bd99679e177c551b980863443ede2a
ufku <ufku@9910.no-reply.drupal.org>
2009-10-13 01:48:26 +0000

    *** empty log message ***

--- admin/bueditor.edit.js
+++ admin/bueditor.edit.js
@@ -1,6 +1,118 @@
 // $Id$
 (function($) {
 
+//Faster alternative to resizable textareas
+Drupal.behaviors.textarea = function(context) {
+  $('textarea.resizable:not(.textarea-processed)', context).each(textArea);
+};
+
+//Faster alternative to sticky headers.
+//Header creation is skipped on load and done once the user scrolls on a table.
+//Fixes tableselect bug where the state of checkbox in the cloned header is not updated.
+Drupal.behaviors.tableHeader = function(context) {
+  var tables =$('table.sticky-enabled:not(.sticky-table)', context).addClass('sticky-table').get();
+  if (tables.length) {
+    if (!bue.tables) {
+      bue.tables = [];
+      $(window).scroll(winScroll).resize(winResize);
+    }
+    bue.tables = bue.tables.concat(tables);
+  }
+};
+
+//process resizable textareas
+var textArea = function(i, T) {
+  var wrp = El('div');
+  var spn = El('span');
+  var grp = El('div');
+  wrp.className = 'resizable-textarea';
+  grp.className = 'grippie';
+  grp.T = T;
+  spn.appendChild(grp);
+  wrp.appendChild(spn);
+  T.parentNode.insertBefore(wrp, T);
+  spn.insertBefore(T, grp);
+  $(grp).mousedown(TDrag);
+  $(T).focus(TFocus).blur(TBlur).addClass('textarea-processed');
+  //grp.style.marginRight = (grp.offsetWidth - T.offsetWidth) +'px';//slow
+};
+
+//start resizing textarea
+var TDrag = function(e) {
+  var $T = $(this.T), $doc = $(document);
+  var doDrag = function(e) {$T.height(Math.max(18, bue.Y + e.pageY));return false;}
+  var noDrag = function(e) {$doc.unbind('mousemove', doDrag).unbind('mouseup', noDrag);$T.css('opacity', 1);}
+  bue.Y = $T.css('opacity', 0.25).height() - e.pageY;
+  $doc.mousemove(doDrag).mouseup(noDrag);
+  return false;
+};
+
+//resize the textarea to its scroll height on focus
+var TFocus = function(e) {
+  var T = this, sH = T.scrollHeight, $T = $(T), tH = $T.height();
+  (T.bueH = sH > tH ? tH : 0) && $T.height(sH);
+}
+
+//restrore the textarea height on blur
+var TBlur = function(e) {
+  var T = this, $T = $(T);
+  if (T.bueH) {
+    var tY = $T.offset().top, wY = $(window).scrollTop(), tH = tY < wY ? $T.height() : 0;
+    $(T).height(T.bueH);
+    //reflect the change in textarea height to the window scroll height if the textarea is above the current view.
+    tH && $(window).scrollTop(wY - (tH - T.bueH));
+  }
+}
+
+//create (table header)
+var createHeader = function(table) {
+  var $fixed = table.$fixed = $(table.cloneNode(false));
+  var $repo = table.$repo = $(El('table')).append(table.tHead.cloneNode(true));
+  $repo.css({visibility: 'hidden', position: 'absolute', left: -1000, top: -1000}).insertBefore(table);
+  $fixed.addClass('sticky-header').css('position', 'fixed')[0].id += '-fixed';
+  return $fixed.insertBefore(table);
+};
+
+//handle window scroll (table header)
+var winScroll = function(e) {
+  var $w = $(window), sX = $w.scrollLeft(), sY = $w.scrollTop();
+  for (var table, i = 0; table = bue.tables[i]; i++) {
+    tableScroll(table, sX, sY);
+  }
+};
+
+//handle window resize (table header)
+var winResize = function(e) {
+  for (var table, i = 0; table = bue.tables[i]; i++) if (table.$fixed && table.$fixed[0].tHead) {
+    table.$fixed.width($(table).width());
+  }
+};
+
+//handle sticky head on scroll (table header)
+var tableScroll = function(table, sX, sY) {
+  var $table = $(table), pos = $table.offset();
+  var minY = pos.top, maxY = minY + $table.height() - $(table.tHead).height() * 2, minX = pos.left;
+  var action = minY < sY && sY < maxY;
+  var $fixed = table.$fixed || false;
+  if (!action && (!$fixed || !$fixed[0].tHead)) return;
+  $fixed = $fixed || createHeader(table);//create when necessary
+  var $repo = table.$repo;
+  if (action) {
+    $fixed.css({visibility: 'visible', top: 0, left: minX-sX});
+    if (!$fixed[0].tHead) {//run once in action
+      var head = table.tHead;
+      $table.prepend($repo[0].tHead);
+      $fixed.append(head).width($table.width());
+    }
+  }
+  else {//run once out of action
+    $fixed.css('visibility', 'hidden');
+    var head = table.tHead;
+    $table.prepend($fixed[0].tHead);
+    $repo.append(head);
+  }
+};
+
 //process initial text(icon) fields. Add selector-opener next to them.
 var iconProc = function(i, inp) {
   var sop = bue.sop.cloneNode(false);
@@ -62,7 +174,7 @@ var iconCreate = function(name) {
   var img = bue.IL[name];
   if (!img) return false;
   if (img.nodeType) return img;
-  img = bue.IL[name] = document.createElement('img');
+  img = bue.IL[name] = El('img');
   img.src = iconUrl(name);
   img.alt = img.title = name;
   return img;
@@ -77,10 +189,10 @@ var createIS = function() {
   var i = 1;
   for (var name in bue.IL) {
     if (i == 6) {
-      tbody.appendChild(row = document.createElement('tr'));
+      tbody.appendChild(row = El('tr'));
       i = 0;
     }
-    row.appendChild(cell = document.createElement('td'));
+    row.appendChild(cell = El('td'));
     cell.title = name;
     cell.onclick = iconClick;
     cell.appendChild(iconCreate(name));
@@ -88,20 +200,17 @@ var createIS = function() {
   }
   //fill in last row
   for(; i < 6; i++) {
-    row.appendChild(document.createElement('td'));
+    row.appendChild(El('td'));
   }
-  table.id = 'icon-selector';
-  $(bue.IS = table).appendTo(document.body).hide();
+  return $(table).attr('id', 'icon-selector').appendTo(document.body).hide()[0];
 };
 
 //table drag adjustment. make value updating simpler and start from 0.
-var alterDrag = function() {
-  var tdrag = Drupal.tableDrag['button-table'];
-  tdrag.updateFields = function() {
+var tableDrag = function() {
+  var tdrag = Drupal.tableDrag && Drupal.tableDrag['button-table'];
+  tdrag && (tdrag.updateFields = function() {
     $('#button-table input.input-weight').each(function(i, field) {field.value = i});
-  };
-  //sort initially to make new buttons sink.
-  tdrag.updateFields();
+  })();//sort initially to make new buttons sink.
 };
 
 //actions for selected buttons
@@ -124,120 +233,26 @@ var selAction = function() {
   }
 };
 
-//process resizable textareas
-var textArea = function(i, T) {
-  var wrp = document.createElement('div');
-  var spn = document.createElement('span');
-  var grp = document.createElement('div');
-  wrp.className = 'resizable-textarea';
-  grp.className = 'grippie';
-  T.className += ' textarea-processed';
-  grp.onmousedown = initDrag;
-  grp.T = T;
-  spn.appendChild(grp);
-  wrp.appendChild(spn);
-  T.parentNode.insertBefore(wrp, T);
-  spn.insertBefore(T, grp);
-  //grp.style.marginRight = (grp.offsetWidth - T.offsetWidth) +'px';//slow
-};
-
-//start resizing textarea
-var initDrag = function(e) {
-  var $T = $(this.T), $doc = $(document);
-  var doDrag = function(e) {$T.height(Math.max(18, bue.Y + e.pageY));return false;}
-  var noDrag = function(e) {$doc.unbind('mousemove', doDrag).unbind('mouseup', noDrag);$T.css('opacity', 1);}
-  bue.Y = $T.css('opacity', 0.25).height() - (e||event).pageY;
-  $doc.mousemove(doDrag).mouseup(noDrag);
-  return false;
-};
-
-//create table header
-var createHeader = function(table) {
-  var $fixed = table.$fixed = $(table.cloneNode(false));
-  var $repo = table.$repo = $(document.createElement('table')).append(table.tHead.cloneNode(true));
-  $repo.css({visibility: 'hidden', position: 'absolute', left: -1000, top: -1000}).insertBefore(table);
-  $fixed.addClass('sticky-header').css('position', 'fixed')[0].id += '-fixed';
-  return $fixed.insertBefore(table);
-};
-
-//handle window scroll (table header)
-var winScroll = function(e) {
-  var $w = $(window), sX = $w.scrollLeft(), sY = $w.scrollTop();
-  for (var table, i = 0; table = bue.tables[i]; i++) {
-    tableScroll(table, sX, sY);
-  }
-};
-
-//handle window resize (table header)
-var winResize = function(e) {
-  for (var table, i = 0; table = bue.tables[i]; i++) if (table.$fixed && table.$fixed[0].tHead) {
-    table.$fixed.width($(table).width());
-  }
-};
-
-//handle sticky head on scroll (table header)
-var tableScroll = function(table, sX, sY) {
-  var $table = $(table), pos = $table.offset();
-  var minY = pos.top, maxY = minY + $table.height() - $(table.tHead).height() * 2, minX = pos.left;
-  var action = minY < sY && sY < maxY;
-  var $fixed = table.$fixed || false;
-  if (!action && (!$fixed || !$fixed[0].tHead)) return;
-  $fixed = $fixed || createHeader(table);//create when necessary
-  var $repo = table.$repo;
-  if (action) {
-    $fixed.css({visibility: 'visible', top: 0, left: minX-sX});
-    if (!$fixed[0].tHead) {//run once
-      var head = table.tHead;
-      $table.prepend($repo[0].tHead);
-      $fixed.append(head).width($table.width());
-    }
-  }
-  else {//run once
-    $fixed.css('visibility', 'hidden');
-    var head = table.tHead;
-    $table.prepend($fixed[0].tHead);
-    $repo.append(head);
-  }
-};
-
-//Faster alternative to resizable textareas
-Drupal.behaviors.textarea = function(context) {
-  $('textarea.resizable:not(.textarea-processed)', context).each(textArea);
-};
-
-//Faster alternative to sticky headers.
-//Header creation is skipped on load and done once the user scrolls on a table.
-//Fixes tableselect bug where the state of checkbox in the cloned header is not updated.
-Drupal.behaviors.tableHeader = function(context) {
-  var tables =$('table.sticky-enabled:not(.sticky-table)', context).addClass('sticky-table').get();
-  if (tables.length) {
-    if (!bue.tables) {
-      bue.tables = [];
-      $(window).scroll(winScroll).resize(winResize);
-    }
-    bue.tables = bue.tables.concat(tables);
-  }
-};
-
-//html to jQuery
-var $html = function(s){return bue.$div.html(s).children()};
-var bue = {};
-
-$(document).ready(function() {
-  //initiate required variables
+//initiate variables and process page elements
+var init = function() {
   bue.IL = Drupal.settings.BUE.iconlist;
   bue.BP = Drupal.settings.basePath;
   bue.IP = bue.BP + Drupal.settings.BUE.iconpath +'/';
-  bue.$div = $(document.createElement('div'));
+  bue.$div = $(El('div'));
   bue.sop = $html('<img class="icon-selector-opener" src="'+ bue.BP +'misc/menu-expanded.png" title="'+ Drupal.t('Select an icon') +'" />')[0];
-  //add icon selector into doc and create a global access for it.
-  createIS();
-  //process icon textfields
-  $('input.input-icon').each(iconProc);
-  //button actions adjustment
-  selAction();
-  //table drag adjustment
-  alterDrag();
-});
+  bue.IS = createIS(); //create icon selector
+  $('input.input-icon').each(iconProc);//process icon textfields
+  selAction();//selected buttons actions
+  tableDrag();//alter table drag
+};
+
+//local container
+var bue = {};
+//create document element
+var El = function(name) {return document.createElement(name)};
+//html to jQuery
+var $html = function(s){return bue.$div.html(s).children()};
+//initiate
+$(document).ready(init);
 
 })(jQuery);
