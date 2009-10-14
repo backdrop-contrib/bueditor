2879dbdb8f43b9b664f59176b715cd299d5535b6
ufku <ufku@9910.no-reply.drupal.org>
2009-10-14 00:46:03 +0000

    - allow custom usage of E.imce.open()

--- library/bue.imce.js
+++ library/bue.imce.js
@@ -4,8 +4,7 @@
 //Requires: bue.popup.js
 (function(E, $) {
 
-var target = {form: {}, value: '', focus: function(){}};
-var I = E.imce = BUE.imce = {field: target};
+var I = E.imce = BUE.imce = {};
 $(function() {I.url = Drupal.settings.BUE.imceURL || ''});
 
 //imce button
@@ -13,42 +12,62 @@ I.button = function(fname, text) {
   return I.url ? '<input type="button" id="bue-imce-button" name="bib" value="'+ (text || Drupal.t('Browse')) +'" onclick="BUE.imce.open(this.form.elements[\''+ fname +'\'])">' : '';
 };
 
+//prepare opening
+I.prepare = function(opt) {
+  I.ready = I.sendto = function(){};
+  if (!opt) return;
+  if (opt.form) {//field
+    I.target = opt;
+    I.ready = I.highlightTarget;
+    I.sendto = I.fillTarget;
+  }
+  else {//options
+    $.isFunction(opt.ready) && (I.ready = opt.ready);
+    $.isFunction(opt.sendto) && (I.sendto = opt.sendto);
+  }
+};
+
 //open imce
-I.open = function(field) {
-  I.field = field || target;
+I.open = function(opt) {
+  I.prepare(opt);
   if (!I.pop) {
     var url = I.url + (I.url.indexOf('?') < 0 ? '?' : '&') + 'app=bue|imceload@bueImceLoad';
     I.pop = BUE.openPopup('bue-imce-pop', Drupal.t('File Browser'), '<iframe src="'+ url +'"></iframe>');
   }
   else {
     I.pop.open();
-    I.hlight();
+    I.ready(I.win, I.pop);
   }
   var $p = $(I.pop), $w = $(window), o = $.browser.opera;
   var h = (o ? $w[0].innerHeight : $w.height()) - $p.height(), w = $w.width() - $p.width();
   $p.css({'top': $w.scrollTop() + Math.max(0, h/2), 'left': Math.max(0, w/2)});
 };
 
-//close imce & process file
+//execute sendto operation
 I.finish = function(file, win) {
-  I.field.value = file.url;
-  var el = I.field.form.elements || {}, val = {'alt': file.name, 'width': file.width, 'height': file.height};
+  I.sendto(file, win, I.pop);
+};
+
+//process file & close imce
+I.fillTarget = function(file, win, pop) {
+  var target = I.target, el = target.form.elements, val = {'alt': file.name, 'width': file.width, 'height': file.height};
+  target.value = file.url;
   for (var i in val) {
     if (el['attr_'+i]) el['attr_'+i].value = val[i];
   }
-  I.pop.close();
-  I.field.focus();
+  pop.close();
+  target.focus();
 };
 
-//highlight file
-I.hlight = function() {
-  I.win.imce.highlight(I.field.value.substr(I.field.value.lastIndexOf('/')+1));
+//highlight file in imce file list
+I.highlightTarget = function(win, pop) {
+  I.win.imce.highlight(I.target.value.substr(I.target.value.lastIndexOf('/')+1));
 };
 
-//imce startup function.
+//imce onload function.
 window.bueImceLoad = function(win) {
   (I.win = win).imce.setSendTo(Drupal.t('Send to editor'), I.finish);
-  I.hlight();
+  I.ready(win, I.pop);
 };
 
 })(BUE.instance.prototype, jQuery);
