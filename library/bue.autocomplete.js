5a6b2e83fcfd53ca7363e454ad2e82efde8816a0
ufku <ufku@9910.no-reply.drupal.org>
2010-03-02 08:02:04 +0000

    - html/bbcode tags are autocompleted by default. - ability to define a AC function for a triggerring string.

--- library/bue.autocomplete.js
+++ library/bue.autocomplete.js
@@ -4,7 +4,19 @@
 //Requires: none
 BUE.preprocess.autocomplete = function(E, $) {
 
-  E.AC = {'<!--': '-->', '<?php': '?>'};
+  //tag completer for html & bbcode
+  var tagComplete = function(E, prefix) {
+    var cursor = E.posSelection().start, content = E.getContent();
+    if (content.substr(cursor - 1, 1) == '/') return;
+    var mate = ({'>': '<', ']': '['})[prefix];
+    var i = content.substr(0, cursor).lastIndexOf(mate);
+    if (i < 0) return;
+    var re = new RegExp('^([a-z][a-z0-9]*)[^\\'+ prefix +']*$');
+    var match = content.substring(i + 1, cursor).match(re);
+    match && E.replaceSelection(mate +'/'+ match[1] + prefix, 'start');
+  };
+
+  E.AC = {'<!--': '-->', '<?php': '?>', '>': tagComplete, ']': tagComplete};
   
   //make sure string preparation runs last as other processes may extend autocomplete list.
   if (!E.index) BUE.postprocess.autocomplete = function(E, $) {
@@ -26,22 +38,33 @@ BUE.preprocess.autocomplete = function(E, $) {
     //disable keycodes that have multi-meaning in opera. 39: hypen-right, 40: parenthesis-down.
     //extend 37:percentage-left, 38:ampersand-up, 33:exclamation-pageup, 34:double quote-pagedown...
     if ($.browser.opera && (code+'').search(/^(39|40)$/) != -1) return;
-    var ac, chr = String.fromCharCode(code);
-    if (!(ac = E.AC[chr])) return;
-    if (!ac.lookback) return E.replaceSelection(ac, 'start');
-    var pos = E.posSelection(), content = E.getContent();
-    for (var lb in ac.lookback) {
-      if (content.substring(pos.start - lb.length, pos.start) == lb) {
-        return E.replaceSelection(ac.lookback[lb], 'start');
+    var handler, suffix, chr = String.fromCharCode(code), prefix = chr;
+    if (!(handler = E.AC[chr])) return;
+    if (!handler.lookback) {
+      suffix = handler;
+    }
+    else {
+      var pos = E.posSelection(), content = E.getContent();
+      for (var lb in handler.lookback) {
+        if (content.substring(pos.start - lb.length, pos.start) == lb) {
+          prefix = lb + prefix;
+          suffix = handler.lookback[lb];
+          break;
+        }
+      }
+      if (suffix === undefined && handler.ins) {
+        suffix = handler.ins
       }
     }
-    if (ac.ins) {
-      return E.replaceSelection(ac.ins, 'start');
+    if ($.isFunction(suffix)) {
+      return suffix(E, prefix);
     }
+    typeof suffix == 'string' && E.replaceSelection(suffix, 'start');
   });
  
 };
 
 
 //Extend or alter autocomplete list in your own postprocess:
-//E.AC['YOUR_AC_TRIGGERING_STRING'] = 'YOUR_AUTO_ENTERED_STRING';
+//E.AC['PREFIX'] = 'SUFFIX';
+//E.AC['PREFIX'] = HANDLER = function(E, PREFIX){...};
